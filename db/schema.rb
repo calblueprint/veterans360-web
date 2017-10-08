1c3a198668bfdcdfc909a211d4ea0822fb6fd720
Ken Chen <lbkchen@gmail.com>
2017-10-08 15:18:19 -0700

    Remove resources from schema so Jason can add.

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< 9f26750b7e5f2766e6e7f5daca204469b2cf19ec
 ActiveRecord::Schema.define(version: 20171010001902) do
-=======
-ActiveRecord::Schema.define(version: 20171008204739) do
->>>>>>> Add resources base model and scaffold and associate with benefactors.
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "veteran_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["veteran_id"], name: "index_friendships_on_veteran_id"
+  end
+
   create_table "resources", force: :cascade do |t|
-<<<<<<< 9f26750b7e5f2766e6e7f5daca204469b2cf19ec
     t.string "file_name"
     t.string "file"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-=======
-    t.string "name"
-    t.string "description"
-    t.string "file"
-    t.string "category"
-    t.string "benefactor_type"
-    t.bigint "benefactor_id"
+  end
+
+  create_table "veterans", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.boolean "on_connect", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["benefactor_type", "benefactor_id"], name: "index_resources_on_benefactor_type_and_benefactor_id"
->>>>>>> Add resources base model and scaffold and associate with benefactors.
   end
 
 end
