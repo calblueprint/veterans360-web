918a9bcb94bdcaaf2acf39ff24c85b867af42283
Ken Chen <lbkchen@gmail.com>
2017-10-08 14:00:57 -0700

    Adds Admin model and associates with resource through benefactor.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< c5a8ebe2cf7fa9d6838935b4584e9cf37fce969f
   resources :veterans
   resources :admins
   resources :partnering_organizations
+=======
+  resources :admins
+>>>>>>> Adds Admin model and associates with resource through benefactor.
   resources :resources
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
