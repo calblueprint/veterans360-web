03d46008bbc5f3559584a08262d3e43263a979f2
Jason Bao <bao.jason.z@gmail.com>
2017-10-08 16:51:23 -0700

    Finally please work

---
+++ test/system/resources_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ResourcesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit resources_url
+  #
+  #   assert_selector "h1", text: "Resource"
+  # end
+end
