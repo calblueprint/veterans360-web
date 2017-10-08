62d902e2969a7fe876094b41c558e25640bb6137
Ken Chen <lbkchen@gmail.com>
2017-10-08 15:18:19 -0700

    Remove resources from schema so Jason can add.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< 9e29f3a21f01d11991afa10759d0cc600a3ed67c
 <<<<<<< 2de87a0936e732f6f383545122ba84ada2a8c2dd
 ActiveRecord::Schema.define(version: 20171013161249) do
 =======
@@ -19,6 +20,9 @@ ActiveRecord::Schema.define(version: 20171010001902) do
 ActiveRecord::Schema.define(version: 20171008204739) do
 >>>>>>> Add resources base model and scaffold and associate with benefactors.
 >>>>>>> Add resources base model and scaffold and associate with benefactors.
+=======
+ActiveRecord::Schema.define(version: 20171010001902) do
+>>>>>>> Remove resources from schema so Jason can add.
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +31,11 @@ ActiveRecord::Schema.define(version: 20171008204739) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+<<<<<<< 9e29f3a21f01d11991afa10759d0cc600a3ed67c
     t.text "description"
+=======
+    t.string "description"
+>>>>>>> Remove resources from schema so Jason can add.
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -41,6 +49,7 @@ ActiveRecord::Schema.define(version: 20171008204739) do
     t.index ["veteran_id"], name: "index_friendships_on_veteran_id"
   end
 
+<<<<<<< 9e29f3a21f01d11991afa10759d0cc600a3ed67c
   create_table "partnering_organizations", force: :cascade do |t|
     t.string "name"
     t.string "phone_number"
@@ -54,24 +63,23 @@ ActiveRecord::Schema.define(version: 20171008204739) do
     t.datetime "updated_at", null: false
   end
 
+=======
+>>>>>>> Remove resources from schema so Jason can add.
   create_table "resources", force: :cascade do |t|
-<<<<<<< 9f26750b7e5f2766e6e7f5daca204469b2cf19ec
     t.string "file_name"
     t.string "file"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-=======
-    t.string "name"
-    t.string "description"
-    t.string "file"
-    t.string "category"
-    t.string "benefactor_type"
-    t.bigint "benefactor_id"
+  end
+
+  create_table "veterans", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.boolean "on_connect", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["benefactor_type", "benefactor_id"], name: "index_resources_on_benefactor_type_and_benefactor_id"
->>>>>>> Add resources base model and scaffold and associate with benefactors.
   end
 
   create_table "veterans", force: :cascade do |t|
