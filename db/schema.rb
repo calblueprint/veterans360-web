93f9bcbb8b560773e25ccf5e7fc009da2c92559b
Jason Bao <bao.jason.z@gmail.com>
2017-10-09 16:06:27 -0700

    fixed missing files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008233734) do
+ActiveRecord::Schema.define(version: 20171009230347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20171008233734) do
   create_table "resources", force: :cascade do |t|
     t.string "fileName"
     t.string "file"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "category"
   end
 
 end
