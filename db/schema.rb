000a6a5f9682d78a43a8f2c25ae280fd62af4cca
Claire Lin <clairelin135@gmail.com>
2017-10-12 21:15:07 -0700

    Partner org model pdate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009230347) do
+ActiveRecord::Schema.define(version: 20171012162054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,14 @@ ActiveRecord::Schema.define(version: 20171009230347) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.integer "role"
+    t.integer "demographic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "resources", force: :cascade do |t|
-    t.string "fileName"
+    t.string "file_name"
     t.string "file"
     t.string "category"
     t.datetime "created_at", null: false
