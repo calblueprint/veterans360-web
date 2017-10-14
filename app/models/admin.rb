e13d25e1d575b31709a92e10c91e965eb72a43ca
Ken Chen <lbkchen@gmail.com>
2017-10-14 12:13:00 -0700

    Readd dependent destroy and resolve all merge conflicts.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -25,5 +25,5 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :resources, as: :benefactor
+  has_many :resources, dependent: :destroy
 end
