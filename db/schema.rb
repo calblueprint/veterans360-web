79bbf5197f8faa40987fffada45dbb275c07abd8
Ken Chen <lbkchen@gmail.com>
2017-10-14 12:31:10 -0700

    Migrates to add partering orgs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013064350) do
+ActiveRecord::Schema.define(version: 20171013161249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,19 @@ ActiveRecord::Schema.define(version: 20171013064350) do
     t.datetime "updated_at", null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["veteran_id"], name: "index_friendships_on_veteran_id"
+  end
+
+  create_table "partnering_organizations", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.string "website"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "role"
+    t.integer "demographic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "resources", force: :cascade do |t|
