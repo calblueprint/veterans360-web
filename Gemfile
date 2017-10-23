e7bf15d16df401f0d5b7b9ce190923e0f7f8133e
Jason Bao <bao.jason.z@gmail.com>
2017-10-23 16:33:18 -0700

    Added carrier wave, edited show to submit form

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem "font-awesome-rails"
 
 gem 'carrierwave', '~> 1.0'
 gem 'fog-aws'
+gem 'figaro'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,12 @@
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     'xxx',                        # required
-    aws_secret_access_key: 'yyy',                        # required
-    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3.example.com',             # optional, defaults to nil
-    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    provider:              'AWS',
+    aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
+    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region:                "us-east-1"
+
   }
-  config.fog_directory  = 'name_of_directory'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
+  config.fog_directory  = ENV.fetch('S3_BUCKET_NAME')
+  config.fog_public     = true
 end
