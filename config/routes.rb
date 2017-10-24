c2d9a0a2feecbe95436459ce52a04c1f3f1160b6
Ken Chen <lbkchen@gmail.com>
2017-10-23 17:12:34 -0700

    Fix devise issue with partnering orgs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 	# get 'partnering_organizations/sign_up' => 'partnering_organizations#new'
 	# post 'partnering_organizations/' => 'partnering_organizations#create'
-  devise_for :partnering_organizations_devise, controllers: { registration: 'partnering_organizations_devise/registration' }
+  devise_for :partnering_organizations, controllers: {
+    registration: 'partnering_organizations/registration'
+  }
   devise_for :veterans
   devise_for :admins
-  devise_for :partnering_organizations
 
   resources :veterans
   resources :admins
