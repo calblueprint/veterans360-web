143e09a9f22ac3bd688b18efabdcc322d3d6194b
Ken Chen <lbkchen@gmail.com>
2017-10-28 15:04:41 -0700

    Adds more signup params to veterans registrations.

--- app/controllers/veterans/registrations_controller.rb
+++ app/controllers/veterans/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Veterans::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -39,14 +39,18 @@ class Veterans::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,
+                                                       :last_name,
+                                                       :roles])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name,
+                                                              :last_name,
+                                                              :roles])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
