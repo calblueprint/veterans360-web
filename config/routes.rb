813f40b23805dc6f62da464e7c6084828121b629
poojnagpal <poojanagpal@berkeley.edu>
2017-11-04 11:23:10 -0700

    Pooja/po signup (#20)
    * schema/puma
    
    * check devise stuff
    
    * Fix devise issue with partnering orgs.
    
    * update po sign up
    
    * made some fixes, not all
    
    * now geocode and devise stuff should be working
    
    * remove puts, and roll back migration for references
    
    * take out extra puts
    
    * port to 3000
    
    * gem file
    
    * fix merge conflicts
    
    * fixed formatting in both these files
    
    * removed .bash_profile
    
    * deletd a bunch of empty files
    
    * changed schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+	# get 'partnering_organizations/sign_up' => 'partnering_organizations#new'
+	# post 'partnering_organizations/' => 'partnering_organizations#create'
+  devise_for :partnering_organizations, controllers: {
+    registrations: 'partnering_organizations/registrations'
+  }
   devise_for :veterans, controllers: {
     sessions: 'veterans/sessions',
     registrations: 'veterans/registrations',
@@ -8,8 +13,7 @@ Rails.application.routes.draw do
   resources :veterans
   resources :admins
   resources :partnering_organizations
-  resources :admins
   resources :resources
-
+  
   root to: 'veterans#index'
 end
