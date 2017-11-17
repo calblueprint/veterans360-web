d55bc924d83b36901e14d9b2b4814aa99689b647
Ken Chen <lbkchen@users.noreply.github.com>
2017-11-17 10:22:35 -0800

    Adds string roles to veterans serializer and fixes a bunch of other bugs. (#35)

--- app/models/partnering_organization.rb
+++ app/models/partnering_organization.rb
@@ -1,17 +1,16 @@
 # == Schema Information
 #
-# Table name: resources
+# Table name: partnering_organizations
 #
-#  name          :string
-#  phone_number  :string
-#  website       :string
-#  address       :string
-#  latitude      :float
-#  longitude     :float
-#  role          :integer
-#  demographic   :integer
 #  id                     :integer          not null, primary key
-#  description            :text
+#  name                   :string
+#  phone_number           :string
+#  website                :string
+#  address                :string
+#  latitude               :float
+#  longitude              :float
+#  role                   :integer
+#  demographic            :integer
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  email                  :string           default(""), not null
@@ -21,9 +20,11 @@
 #  remember_created_at    :datetime
 #  sign_in_count          :integer          default(0), not null
 #  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime`
+#  last_sign_in_at        :datetime
 #  current_sign_in_ip     :inet
 #  last_sign_in_ip        :inet
+#
+
 class PartneringOrganization < ApplicationRecord
 	geocoded_by :address
 	after_validation :geocode
