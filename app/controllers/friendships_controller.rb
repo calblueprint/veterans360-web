351cb152dd211621f55c9995b8119aba6fa13977
Ken Chen <lbkchen@gmail.com>
2017-11-18 15:15:32 -0800

    Adds initial friendships controller.

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,24 @@
+class FriendshipsController < ApplicationController
+
+  def create
+    veteran_id = friendship_params[:veteran_id]
+    friend_id = friendship_params[:friend_id]
+    veteran = Veteran.find(veteran_id)
+    @friendship = veteran.friendships.build(friend_id: friend_id)
+    if @friendship.save
+      render json: @friendship
+    else
+      render json: @resource.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(
+      :veteran_id,
+      :friend_id,
+    )
+  end
+
+end
