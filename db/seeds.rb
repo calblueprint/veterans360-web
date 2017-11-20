06b017c0d857148fa04d5d44a5a2a91b90aff314
Ken Chen <lbkchen@gmail.com>
2017-11-19 17:17:55 -0800

    Add partering orgs seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@
 def create_veterans
   veterans = []
   rng = Random.new(934857239324211115710)
+  num_roles = Veteran::ROLE_KEYS.count
 
+  # Create 10 Veterans with random names and some random roles
   10.times do |i|
     veteran = Veteran.create(
       first_name: FFaker::Name.first_name,
@@ -18,13 +20,14 @@ def create_veterans
       email: "veteran#{i}@gmail.com",
       password: 'password',
       password_confirmation: 'password',
-      roles: Array(0..5).sample(rng.rand(6) + 1)
+      roles: Array(0...num_roles).sample(rng.rand(num_roles) + 1)
     )
     veterans << veteran
   end
 
+  # Create 0-5 friendships for each veteran
   veterans.each do |veteran|
-    rng.rand(5).times do |i|
+    rng.rand(6).times do |i|
       friendship = Friendship.new(
         veteran_id: veteran.id,
         friend_id: rng.rand(10) + 1
@@ -37,4 +40,30 @@ def create_veterans
 
 end
 
-create_veterans 
+
+# Create Partering Orgs
+def create_partering_orgs
+  rng = Random.new(934857239324211115710)
+  10.times do |i|
+    po = PartneringOrganization.create(
+      name: FFaker::Company.name,
+      email: "po#{i}@gmail.com",
+      password: 'password',
+      password_confirmation: 'password',
+      phone_number: FFaker::PhoneNumber.short_phone_number,
+      website: FFaker::Product.product.gsub(/\s+/, "").downcase + '.com',
+      address: FFaker::AddressUS.street_address + ', ' +
+               FFaker::AddressUS.city + ', ' +
+               FFaker::AddressUS.state_abbr + ' ' +
+               FFaker::AddressUS.zip_code,
+      latitude: FFaker::Geolocation.lat,
+      longitude: FFaker::Geolocation.lng,
+      role: rng.rand(PartneringOrganization.roles.count),
+      demographic: rng.rand(PartneringOrganization.demographics.count),
+    )
+  end
+end
+
+
+create_veterans
+create_partering_orgs
