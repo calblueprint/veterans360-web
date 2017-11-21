6ae13e65899bec337452d44ac0c201cc58c4ca64
Claire Lin <clairelin135@gmail.com>
2017-11-21 09:12:25 -0800

    remove upvotes

--- db/schema.rb
+++ db/schema.rb
@@ -81,15 +81,6 @@ ActiveRecord::Schema.define(version: 20171116014836) do
     t.index ["owner_type", "owner_id"], name: "index_resources_on_owner_type_and_owner_id"
   end
 
-  create_table "upvotes", force: :cascade do |t|
-    t.bigint "veteran_id"
-    t.bigint "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["resource_id"], name: "index_upvotes_on_resource_id"
-    t.index ["veteran_id"], name: "index_upvotes_on_veteran_id"
-  end
-
   create_table "veterans", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -117,6 +108,4 @@ ActiveRecord::Schema.define(version: 20171116014836) do
     t.index ["reset_password_token"], name: "index_veterans_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "upvotes", "resources"
-  add_foreign_key "upvotes", "veterans"
 end
