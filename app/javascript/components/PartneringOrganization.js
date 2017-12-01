b3ad51be248b3ef4084649572b6bee6abc046dd9
Ken Chen <lbkchen@gmail.com>
2017-11-30 16:31:49 -0800

    Pooja's thing is fixed.

---
+++ app/javascript/components/PartneringOrganization.js
@@ -0,0 +1,11 @@
+import React from "react"
+import PropTypes from "prop-types"
+class PartneringOrganization extends React.Component {
+  render () {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default PartneringOrganization
---
+++ app/javascript/components/PartneringOrganizationProfile.js
@@ -0,0 +1,23 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+/**
+*
+@prop partnering_organization -partnering organization object
+*/
+
+class PartneringOrganizationProfile extends React.Component {
+	render() {
+			return (
+				<div>HELLO THERE</div>
+			);
+	}
+}
+
+export default PartneringOrganizationProfile;
+
+	// PartneringOrganizationProfile.propTypes = {
+	// 	partnering_organization: React.propTypes.object,
+	// }
+
+	// export default PartneringOrganizationProfile
