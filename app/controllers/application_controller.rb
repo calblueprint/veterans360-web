7716d68c2a30c47c720701bd7c28770b33f9855a
Nikita Rau <raun@berkeley.edu>
2017-11-30 19:37:39 -0800

    reroute after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    case
+    when resource.is_a?(Admin)
+      "/admins/#{current_admin.id}"
+    when resource.is_a?(PartneringOrganization)
+      partnering_organization_path
+    else
+      super
+    end
+  end
 end
