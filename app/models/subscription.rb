6956267fbed0f73ed6e6bb9b4af4ab50d06dd964
Ken Chen <lbkchen@gmail.com>
2017-12-02 13:12:59 -0800

    Adds subscription field to serializer.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,6 @@
 class Subscription < ApplicationRecord
   belongs_to :veteran
   belongs_to :partnering_organization
+
+  validates_uniqueness_of :veteran_id, scope: :partnering_organization_id
 end
