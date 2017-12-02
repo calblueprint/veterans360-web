622a7da38bb4d34936026ebc7ee582ebe2877660
Claire Lin <clairelin135@gmail.com>
2017-12-02 00:17:10 -0800

    update filter (#44)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128020202) do
+ActiveRecord::Schema.define(version: 20171201052031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 20171128020202) do
   create_table "resources", force: :cascade do |t|
     t.string "file_name"
     t.string "file"
-    t.string "category"
+    t.integer "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "owner_type"
