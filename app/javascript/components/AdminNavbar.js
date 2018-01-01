ac640a5e4bf04fbd8cbacfc1e013c034a71c323f
Jason Bao <bao.jason.z@gmail.com>
2017-12-31 16:42:06 -0800

    DRY cleanups / got rid of unused code

--- app/javascript/components/AdminNavbar.js
+++ app/javascript/components/AdminNavbar.js
@@ -6,16 +6,16 @@ class AdminNavbar extends React.Component {
   render() {
     return (
       <div>
-        <nav class="pt-navbar .modifier">
-          <div class="pt-navbar-group pt-align-left">
-            <div class="pt-navbar-heading">Veterans 360</div>
+        <nav className="pt-navbar .modifier">
+          <div className="pt-navbar-group pt-align-left">
+            <div className="pt-navbar-heading">Veterans 360</div>
           </div>
-          <div class="pt-navbar-group pt-align-right">
-            <a href={`/admins/applications`}><button class="pt-button pt-minimal pt-icon-comparison">Applications</button></a>
-            <a href={`/admins/resources`}><button class="pt-button pt-minimal pt-icon-document">Resources</button></a>
-            <span class="pt-navbar-divider"></span>
-            <button class="pt-button pt-minimal pt-icon-user">Profile</button>
-            <button class="pt-button pt-minimal pt-icon-log-out">Log out</button>
+          <div className="pt-navbar-group pt-align-right">
+            <a href={`/admins/applications`}><button className="pt-button pt-minimal pt-icon-comparison">Applications</button></a>
+            <a href={`/admins/resources`}><button className="pt-button pt-minimal pt-icon-document">Resources</button></a>
+            <span className="pt-navbar-divider"></span>
+            <button className="pt-button pt-minimal pt-icon-user">Profile</button>
+            <button className="pt-button pt-minimal pt-icon-log-out">Log out</button>
           </div>
         </nav>
       </div>
--- app/javascript/components/ApplicationModal.js
+++ app/javascript/components/ApplicationModal.js
@@ -11,72 +11,51 @@ class ResourceModal extends React.Component {
 
   }
 
-  render_approved_resource() {
-    return (
-      <Card interactive={true} elevation={Card.ELEVATION_TWO}>
-        <p>
-          Name: {this.props.application.name}
-        </p>
-        <p>
-          Phone Number: {this.props.application.phone_number}
-        </p>
-        <p>
-          Web Site: {this.props.application.website}
-        </p>
-        <p>
-          Role: {this.props.application.role}
-        </p>
-        <p className="approved-status">
-          Approved Application!
-        </p>
-        <a href={`/partnering_organizations/` + this.props.application.id} data-method="delete" data-confirm="This will delete the resource, are you sure?">
-          <Button className='reject-po'>
-            Delete
-          </Button>
-        </a>
-      </Card>
-    )
-  }
-
-  render_unapproved_resource() {
-    return (
-      <Card interactive={true} elevation={Card.ELEVATION_TWO}>
-        <p>
-          Name: {this.props.application.name}
-        </p>
-        <p>
-          Phone Number: {this.props.application.phone_number}
-        </p>
-        <p>
-          Web Site: {this.props.application.website}
-        </p>
-        <p>
-          Role: {this.props.application.role}
-        </p>
-        <p className="pending-status">
-          Pending Approval
-        </p>
-        <a href={`/partnering_organizations/` + this.props.application.id + `/approve`} data-method="patch">
-          <Button className='approve-po'>
-            Approve
-          </Button>
-        </a>
-        <a href={`/partnering_organizations/` + this.props.application.id} data-method="delete" data-confirm="This will delete the resource, are you sure?">
-          <Button className='reject-po'>
-            Reject
-          </Button>
-        </a>
-      </Card>
-    )
-  }
-
   render() {
-    if (this.props.application.approval_status) {
-      return this.render_approved_resource()
-    }
-    else {
-      return this.render_unapproved_resource()
-    }
+    return (
+      <Card interactive={true} elevation={Card.ELEVATION_TWO}>
+        <p>
+          Name: {this.props.application.name}
+        </p>
+        <p>
+          Phone Number: {this.props.application.phone_number}
+        </p>
+        <p>
+          Web Site: {this.props.application.website}
+        </p>
+        <p>
+          Role: {this.props.application.role}
+        </p>
+        {this.props.application.approval_status ? (
+          <div>
+            <p className="approved-status">
+              Approved Application!
+            </p>
+            <a href={`/partnering_organizations/` + this.props.application.id} data-method="delete" data-confirm="This will delete the resource, are you sure?">
+              <Button className='reject-po'>
+                Delete
+              </Button>
+            </a>
+          </div>
+        ) : (
+          <div>
+            <p className="pending-status">
+              Pending Approval
+            </p>
+            <a href={`/partnering_organizations/` + this.props.application.id + `/approve`} data-method="patch">
+              <Button className='approve-po'>
+                Approve
+              </Button>
+            </a>
+            <a href={`/partnering_organizations/` + this.props.application.id} data-method="delete" data-confirm="This will delete the resource, are you sure?">
+              <Button className='reject-po'>
+                Reject
+              </Button>
+            </a>
+          </div>
+        )}
+      </Card>
+    )
   }
 }
 
@@ -87,7 +66,6 @@ ResourceModal.propTypes = {
     phone_number: PropTypes.string.isRequired,
     approval_status: PropTypes.bool.isRequired,
     website: PropTypes.string.isRequired,
-    phone_number: PropTypes.number.isRequired,
   })
 }
 
