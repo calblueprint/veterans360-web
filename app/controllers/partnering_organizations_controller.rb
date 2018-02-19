ceb3cc29092897ae0bc5b88c71334592cb16c7ce
Greta Huang <gretahuang@berkeley.edu>
2018-02-18 17:47:48 -0800

    Finish authentication protections.'

--- app/controllers/partnering_organizations_controller.rb
+++ app/controllers/partnering_organizations_controller.rb
@@ -1,7 +1,6 @@
 class PartneringOrganizationsController < ApplicationController
   before_action :set_partnering_organization, only: [:show, :edit, :update, :destroy, :approve]
-  # before_action :authenticate!
-  load_and_authorize_resource
+  before_action :authenticate_partnering_organization!
 
   # GET /partnering_organizations
   # GET /partnering_organizations.json
