32f37b339276fcf7992fb5b0cdefdc4898a8e686
Greta Huang <gretahuang@berkeley.edu>
2018-02-19 23:55:25 -0800

    Increase granularity of abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,15 +3,17 @@ class Ability
 
   def initialize(user)
     if user.is_a?(PartneringOrganization)
-      user ||= PartneringOrganization.new
+      can :read, Resource
+      can :manage, Resource, user_id: user.id 
       can :read, PartneringOrganization
       can :manage, PartneringOrganization
+      can :read, Veteran
     elsif user.is_a?(Veteran)
-      user ||= Veteran.new
+      can :read, Resource
+      can :read, PartneringOrganization
       can :read, Veteran
       can :manage, Veteran
     elsif user.is_a?(Admin)
-      # user ||= Admin.new
       can :read, :all
       can :manage, :all
     end
