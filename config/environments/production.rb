cc7c3fc95bd33b8b54c623a32ec05f9dfdfb248b
Jason Bao <bao.jason.z@gmail.com>
2018-03-03 12:06:13 -0800

    deleted line in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,6 @@ Rails.application.configure do
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
-  config.web_console.whitelisted_ips = ‘10.142.182.161’
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
