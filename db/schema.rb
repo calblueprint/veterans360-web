f31aa39a1f4d7fa1e339c6aef660f20a32a58e09
Jason Bao <bao.jason.z@gmail.com>
2018-04-04 01:10:55 -0700

    first step in adding category sections (added to schema and created enum)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326105312) do
+ActiveRecord::Schema.define(version: 20180404080851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20180326105312) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "section"
   end
 
   create_table "friendships", force: :cascade do |t|
